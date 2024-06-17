caec2d496207304ae0b9036e3d35ebdf13a12fc8
diebomb <diego.bombin@estudiantes.uva.es>
2024-06-18 00:27:45 +0200

    INIT

--- intento/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site:'https://diegobombinsanz.github.io/Portafolio',
-  base:'/Portafolio',
-});
